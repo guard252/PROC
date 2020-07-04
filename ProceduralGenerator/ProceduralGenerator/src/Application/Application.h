5694bc2fed15957eecf3696128f74c68248c6808
Danylo Sadivnychyi <Danylotv43@gmail.com>
2020-07-04 19:46:31 +0300

    Implemented Application class

---
+++ ProceduralGenerator/ProceduralGenerator/src/Application/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <memory>
+#include "../Window/Window.h"
+
+class Application
+{
+	std::unique_ptr<Window> m_window;
+	bool m_is_running = true;
+	
+protected:
+	virtual void OnUpdate() = 0;
+public: 
+	Application(std::string name);
+	~Application() = default;
+
+	void Run();
+	
+
+};
